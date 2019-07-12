446fc7e989b24860064b23d7d71fe0eaf41b2aa2
Fikir Bahçıvanı <51836340+fikirbahcivani@users.noreply.github.com>
2019-07-12 12:27:47 +0300

    Update about.php

--- about.php
+++ about.php
@@ -32,6 +32,109 @@
     <!--================ End Home Banner Area =================-->
 
 	<!--================ Start About Us Area =================-->
+	
+	<p>&nbsp;<strong>Sizden Gelen Sorularla Fikir Bah&ccedil;ıvanı&rsquo;nı Tanıyalım! </strong></p>
+<p>&nbsp;</p>
+<p>&nbsp; &Ouml;ğrencilerin yaşadığı sorunları en yakından bilen, yaşayan ve bu durumu d&uuml;zeltmek i&ccedil;in <strong><em>Geleceğe Dokunmak İ&ccedil;in El Ele</em></strong> sloganı ile &uuml;niversite &ouml;ğrencileri <strong>Fikir Bah&ccedil;ıvanı Kitle Fonlama Platformu</strong>&rsquo;nu kurmuşlardır. Bu aşamada yaşadıkları sorunlar, engeller yine &ouml;ğrenci olmalarından kaynaklı ortaya &ccedil;ıkmış; bu durumu aşmaları i&ccedil;in onlara g&uuml;venen, bir şeyleri başarmalarını isteyen kişilerin yardımı ve desteği ile bug&uuml;n karşınızda diğer &ouml;ğrencilerin bu sorunlarını ortadan kaldırmak, başarılı olmalarını sağlamak ve bir değer &uuml;retmelerine yardımcı olmak amacı ile platform olarak karşınızdalar. Bu aşamada ilk olarak platformun daha iyi hale gelerek daha &ccedil;ok &ouml;ğrenciye ulaşabilmesi ve sistemin daha &ccedil;ok kişinin bilmesi i&ccedil;in ilk olarak kendileri i&ccedil;in sizlerden destek istiyorlar. Bunun i&ccedil;in ise ilk seslendikleri kişiler elbette onların yaşadığı sorunları en iyi tahmin edecek &ouml;ğrenci arkadaşları. Sloganımızın ger&ccedil;ek anlamına daha iyi b&uuml;r&uuml;nmesi ve amacımız doğrultusunda daha hızlı ilerlemesi i&ccedil;in daha fazla kişiye ulaşmak istiyoruz. Ne kadar &ccedil;ok kişiyi kitlemizde bulundurursak, o kadar g&uuml;&ccedil;l&uuml; ve başarılı oluruz. Ama&ccedil;larımızın ger&ccedil;ekleşebilmesi i&ccedil;in sizin desteğinize sonuna kadar ihtiyacımız var. Bunun i&ccedil;in tek yapmanız gereken Bize Destek Olun!&nbsp;&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp; Fikir Bah&ccedil;ıvanı &ouml;ğrenciler ve sosyal sorumluluk projeleri i&ccedil;in oluşturulmuş bir kitle fonlama platformudur. Yaptığımız işi daha iyi anlayabilmeniz i&ccedil;in kitle fonlamasından bahsetmek istiyoruz.</p>
+<p>&nbsp;</p>
+<p>&nbsp;&nbsp;Kitle fonlaması (crowdfunding) temelinde bir imece usul&uuml;d&uuml;r. Peki bu imece usul&uuml;nde sistem nasıl işlemektedir? Fikir Bah&ccedil;ıvanı platformunu &ouml;rnek vermek gerekirse; &ouml;ğrenciler ve sosyal sorumluluk projesi olan kişiler, projelerini platforma g&ouml;nderirler. Proje denetim kurulu değerlendirmesi sonrası (Eğer proje denetim kurulu tarafından onaylanırsa.) proje platformda yayımlanır. Kitleler belirlenen &ouml;d&uuml;ller karşılığında projeye destek atarlar. Daha sonra proje sahibi belirlediği &uuml;r&uuml;nleri, belirlenen tarihlerde kendisini destekleyen kişilere g&ouml;nderir. T&uuml;m yapı kısaca bu şekildedir.</p>
+<p>&nbsp;Diğer bir yandan kitle fonlaması 3 modele sahiptir. Bunlar;</p>
+<p>&nbsp; 1) &Ouml;d&uuml;l bazlı kitle fonlama.</p>
+<p>&nbsp; 2) Bağış bazlı kitle fonlama.</p>
+<p>&nbsp;&nbsp;3) Yatırım bazlı kitle fonlama.</p>
+<p>&nbsp; &nbsp;Bu ilk iki yapı T&uuml;rkiye&rsquo;de uygulanmaktadır. &Uuml;&ccedil;&uuml;nc&uuml; yapı i&ccedil;inse mevzuat hazırlık aşamasındadır. Mevzuatın SPK tarafından hazırlanarak onaylanması ile birlikte T&uuml;rkiye&rsquo;de yatırım bazlı kitle fonlama da karşımıza &ccedil;ıkacaktır.</p>
+<p>&nbsp;</p>
+<p>&nbsp; &nbsp;Kısaca kitle fonlamasından bahsettiğimize g&ouml;re şimdi sizden gelen sorularla Fikir Bah&ccedil;ıvanı&rsquo;nı daha iyi tanıyalım.</p>
+<p>&nbsp;</p>
+<ul>
+<li><strong>Peki Fikir Bah&ccedil;ıvanı hangi modeli uygulamaktadır? </strong></li>
+</ul>
+<p>&nbsp;&nbsp; Biz &ouml;d&uuml;l bazlı kitle fonlama sistemini uygulamaktayız. Fakat buradaki asıl ama&ccedil;lanan &ouml;d&uuml;l manevi bir &ouml;d&uuml;ld&uuml;r. Neden mi? &Ccedil;&uuml;nk&uuml; platformumuzda &ouml;ğrencilerin projeleri ve sosyal sorumluluk projeleri bulunmaktadır.&nbsp; Aynı zamanda proje sahiplerinin belirlediği &ouml;d&uuml;llerde bulunmaktadır ve bu &ouml;d&uuml;ller projeden projeye değişmektedir. T&uuml;m bu belirtilen sistemin iyi şekilde ilerlemesi ile birlikte ilerleyen zamanlarda bağış bazlı kitle fonlamasına ge&ccedil;meyi planlamaktayız. Aynı zamanda SPK tarafından mevzuatın bitmesi ile beraber yatırım bazlı kitle fonlama modeline ge&ccedil;mekte planlarımız arasındadır.</p>
+<p>&nbsp;</p>
+<ul>
+<li><strong>&Ouml;d&uuml;l bazlı kitle fonlaması kısaca nedir? </strong></li>
+</ul>
+<p>&nbsp; Bu kitle fonlama modelinde proje sahibi belirlediği &ouml;d&uuml;lleri kendisini destekleyen kişilere g&ouml;ndermektedir. &Ouml;d&uuml;ller proje sahibinin tercihine g&ouml;re belirlendiği i&ccedil;in projeden projeye farklılık g&ouml;stermektedir. Bu modelin daha iyi anlaşılması i&ccedil;in bir &ouml;rnek vermek istiyoruz. Proje sahibi kendisini 10 TL destekleyen kişilere eğer ortaya &ccedil;ıkaracağı &uuml;r&uuml;n&uuml; g&ouml;ndermek istiyorsa; 10 TL desteğiniz karşılığında belirtilen tarihte proje sahibinin ortaya &ccedil;ıkardığı &uuml;r&uuml;ne sahip olursunuz.</p>
+<p>&nbsp;</p>
+<ul>
+<li><strong>Fikir Bah&ccedil;ıvanı&rsquo;na herkes proje yollayabilir mi? </strong></li>
+</ul>
+<p>&nbsp; Buna cevabımız bir m&uuml;ddet boyunca bir sosyal sorumluluk projeniz var ise evet, diğer projeler i&ccedil;inse sadece &ouml;ğrencilerin projelerini değerlendirmekteyiz. Yaşı 18&rsquo;den k&uuml;&ccedil;&uuml;k olan &ouml;ğrenciler ise veli izni ile birlikte proje yollayabilirler.</p>
+<p>&nbsp;</p>
+<ul>
+<li><strong>Platformdaki t&uuml;m projeler g&uuml;venilir mi, bu g&uuml;veni nasıl sağlıyorsunuz? </strong></li>
+</ul>
+<p>&nbsp;&nbsp; Bize g&ouml;nderilen t&uuml;m projeler denetim kurulu tarafından incelenmekte ve onaylanmaktadır. Bu denetim kurulumuzda akademisyenler, &ouml;ğretmenler ve mentorler bulunmaktadır. Platform olarak sadece &ouml;ğrencilerin projelerini değerlendirdiğimiz i&ccedil;in &ouml;ğrencilerin okulları ile de iletişim halindeyiz. &Ouml;ğrencilerimize sonuna kadar g&uuml;venmekte ve onların başarısının bizim başarımız olduğuna inanmaktayız.</p>
+<p>&nbsp;</p>
+<ul>
+<li><strong>Fikir Bah&ccedil;ıvanı&rsquo;nda fon miktarı ne kadar bir oranda toplanırsa başarılı sayılır? </strong></li>
+</ul>
+<p>&nbsp; &nbsp;Platformumuzda belirlenen fon miktarı %75 oranında başarı sağlarsa, toplanan fonlar sahibine verilmektedir. %75 oranının altında kalan fonlar ise başarısız sayılmakta ve projeyi destekleyen kişilerin saksısına materyaller geri g&ouml;nderilmektedir.</p>
+<p>&nbsp;</p>
+<ul>
+<li><strong>Sosyal sorumluluk projelerini ve &ouml;ğrencileri desteklemek beni mutlu ediyor. Hedefledikleri t&uuml;m fon miktarını g&ouml;nderebilir miyim? </strong></li>
+</ul>
+<p>&nbsp; Elbette g&ouml;nderebilirsiniz. Bu bizi ve proje sahiplerini &ccedil;ok mutlu edecektir. Siz t&uuml;m miktarı g&ouml;nderseniz dahi proje fon toplama s&uuml;resinin bitmesini bekleyecek ve sizin gibi insanlar tarafından desteklenmeye devam edecektir.</p>
+<p>&nbsp;</p>
+<ul>
+<li><strong>Proje i&ccedil;in fon toplama s&uuml;resi ne kadar? </strong></li>
+</ul>
+<p>&nbsp; Platformumuzda fon toplama s&uuml;resi 90 g&uuml;n olarak belirlenmiştir. Bu s&uuml;re boyunca proje hedeflediği fon miktarını ge&ccedil;se bile fonlanmaya devam edecektir.</p>
+<p>&nbsp;</p>
+<ul>
+<li><strong>Belirlenen s&uuml;re sonunda toplanan fon miktarı, hedeflenen fon miktarını ge&ccedil;erse proje sahibi sadece hedeflediği miktarı mı alacak? </strong></li>
+</ul>
+<p>&nbsp; Platform olarak tek incelediğimiz durum toplanan fonun %75 oranını ge&ccedil;ip ge&ccedil;memesi, eğer %75 oranını ge&ccedil;tiyse, proje toplamda ne kadar fon topladıysa o miktarı alacaktır.</p>
+<p>&nbsp;</p>
+<ul>
+<li><strong>Projeyi &ccedil;ok beğendim ve yatırımcı olmak istiyorum ne yapmam gerekiyor? </strong></li>
+</ul>
+<p>&nbsp;&nbsp;&nbsp;Bize bu konu hakkında ulaşarak bilgi verebilirsiniz, bunun i&ccedil;in <a href="mailto:destek@fikirbahcivani.com">destek@fikirbahcivani.com</a> adresine mesaj atabilirsiniz.</p>
+<p>&nbsp;</p>
+<ul>
+<li><strong>Desteklediğim proje başarılı olamazsa yolladığım materyale (Toprak,su damlası,g&uuml;l,yonca vb.) ne olacak? </strong></li>
+</ul>
+<p>&nbsp;&nbsp;&nbsp;Proje fon miktarı %75 oranında başarı sağlamaz ise belirlenen s&uuml;re sonunda yolladığınız materyaller saksınıza geri g&ouml;nderilecektir. Burada saksınıza g&ouml;nderilmesindeki asıl ama&ccedil; diğer &ouml;ğrencilerinde sizin desteğine ihtiyacınız olması eğer aldığınız materyallerin karşılığını geri iade edilmesini istiyorsanız bize <a href="mailto:destek@fikirbahcivani.com">destek@fikirbahcivani.com</a> mail adresinden ulaşarak talebinizi iletebilirsiniz. Diğer bir yandan saksınızda bulunan materyalleri bir (1) sene boyunca herhangi bir &ouml;ğrenci ya da sosyal sorumluluk projesine yollamazsanız, sistem otomatik olarak o an bulunan projelere eşit olarak materyallerinizi g&ouml;nderecektir. Siz saksınızı unutmuş olabilirsiniz fakat &ouml;ğrenciler o an bile bir şeyler başarmak i&ccedil;in &ccedil;alışmakta ve sizin desteğinizi beklemektedir.</p>
+<p>&nbsp;</p>
+<ul>
+<li><strong>Desteklediğim kişi &ouml;d&uuml;l&uuml;m&uuml; yollamazsa ne olacak? </strong></li>
+</ul>
+<p>&nbsp;&nbsp;Desteklediğiniz kişilerin iletişim bilgileri platformda g&ouml;z&uuml;kmektedir. Bu sorunuzu ilk olarak proje sahibine aktarırsanız, daha detaylı bilgi alabilirsiniz. Eğer desteklediğiniz kişiye ulaşamıyorsanız bizimle iletişime ge&ccedil;erek bu konuda destek alabilirsiniz.</p>
+<p>&nbsp;</p>
+<ul>
+<li><strong>Desteklediğim kişilerin ger&ccedil;ekten aldıkları fonu proje i&ccedil;in harcadıklarını nasıl bileceğim? </strong></li>
+</ul>
+<p>&nbsp; Proje sahiplerinden 3 ayda bir projenin hangi aşamada olduğu, neler yaptıkları, aldıkları fonu nerelerde harcadıkları gibi konularda bir video &ccedil;ekim yapmalarını ve kendisini destekleyen kişilere g&ouml;ndermelerini istiyoruz. Aynı zamanda Fikir Bah&ccedil;ıvanı olarak projelerin</p>
+<p>ilerleyişleri konusunda d&uuml;zenli olarak bilgi almakta ve incelemekteyiz. Eğer proje sahibi aldığı fonu projesi i&ccedil;in kullanmadıysa kendisi hakkında hukuksal işlem başlatıyoruz.</p>
+<p>&nbsp;</p>
+<ul>
+<li><strong>Neden Fikir Bah&ccedil;ıvanı ismi? </strong></li>
+</ul>
+<p>&nbsp; Girişimcilik ekosisteminde fikirlere tohum denilmektedir. Fikir eğer bir tohumsa, bir fikrin ger&ccedil;eğe d&ouml;n&uuml;şmesi, gelişmesi ve başarılı olması i&ccedil;in &ccedil;aba g&ouml;steren herkes bir bah&ccedil;ıvandır. Bu mantık doğrultusunda 3 yıl &ouml;nce Fikir Bah&ccedil;ıvanı ismi ortaya &ccedil;ıkmıştır.</p>
+<p>&nbsp;</p>
+<ul>
+<li><strong>Fikir Bah&ccedil;ıvanı ka&ccedil; yıldır hizmet vermektedir? </strong></li>
+</ul>
+<p>&nbsp; Kitle fonlama sistemi bizim i&ccedil;in yeni bir heyecan. Daha &ouml;nce platform olarak girişimcilik ekosistemi hakkında bilgi veren, video &ccedil;ekimleri yapan ve girişimcilerle r&ouml;portaj yapan bir platformduk. Daha sonra girişimcilik ekosistemi i&ccedil;erisinde &ouml;ğrencilerin yaşadığı sorunları g&ouml;rmemiz ile birlikte, &ouml;ğrenciler i&ccedil;in &ouml;d&uuml;l bazlı kitle fonlama sistemine ge&ccedil;tik. 3 yıldır edindiğimiz bilgi ve tecr&uuml;be ile şu an kitle fonlama platformu olarak karşınızdayız biraz ge&ccedil; kaldığımızın farkındayız bu durumun asıl sebebi ise ekonomik sebepler bundan kaynaklı daha hızlı ilerlememiz ve daha &ccedil;ok &ouml;ğrencinin hayatına dokunabilmemiz i&ccedil;in sizlerden Fikir Bah&ccedil;ıvanı i&ccedil;in desteklerinizi bekliyoruz. Aynı zamanda bir kitle fonlaması değil aynı zamanda girişimcilerin ortaya &ccedil;ıkardığı &uuml;r&uuml;nleri satabileceği (Manav), kendilerini geliştirmek i&ccedil;in eğitimler alabileceği (Bu sayfamız şu an yapım aşamasında) bir yapıyız da. Zaman i&ccedil;erisinde platformumuzu daha iyi hale getirerek farklı alanlarda da hizmet vereceğiz. Fakat şu anlık kitle fonlamasına odaklandık ve sistemi daha iyi hale getirmek i&ccedil;in t&uuml;m g&uuml;c&uuml;m&uuml;zle &ccedil;alışmalar s&uuml;rd&uuml;rmekteyiz. &nbsp;</p>
+<p>&nbsp;</p>
+<ul>
+<li><strong>Sisteminiz diğer kitle fonlama platformlarının sisteminden farklı bu konu hakkında bilgi verir misiniz? </strong></li>
+</ul>
+<p>&nbsp; Kitle fonlamasına yeni bir soluk getirerek k&uuml;&ccedil;&uuml;k miktarlarda da fon toplamayı ama&ccedil;lamaktayız. Bu mantık doğrultusunda bir oyunlaştırma geliştirdik ve sistemi ona g&ouml;re hazırladık. Manav kısmında karşınıza &ccedil;ıkan t&uuml;m materyaller bu oyunlaştırmanın bir &uuml;r&uuml;n&uuml;d&uuml;r. &Ouml;rnek olarak projeye ya da projelere 10 kuruş değerinde destek olmak istiyorsunuz. Tek yapmanız gereken 10 kuruş değerindeki Toprak materyaline sahip olmanız fakat burada bazı nedenlerden dolayı 10 kuruş değerindeki Toprak materyalinden en az 10 tane sahip olabilirsiniz. &Ouml;deme işlemini tamamladıktan sonra 10 adet Toprak materyali saksınıza y&uuml;klenecektir. Bu 10 adet Toprak materyalini farklı projelere g&ouml;nderebilirsiniz ( Her biri 10 kuruş değerinde sayılacaktır.) Diğer bir yandan Tohum materyalinden (25 kuruş değerinde) en az 4 ve Su Damlası&rsquo;ndan (50 kuruş değerinde) &nbsp;en az 2 tane sahip olabilirsiniz, diğer materyallerde b&ouml;yle bir durum ge&ccedil;erli değildir. &nbsp;Materyaller t&uuml;m projeler i&ccedil;in ge&ccedil;erli olan destek miktarlarıdır. &Ouml;ğrenciler ya da sosyal sorumluluk projesini y&uuml;r&uuml;ten kişiler bu destek miktarları i&ccedil;in &ouml;d&uuml;llerini belirlerler. Yolladığınız materyalin &ouml;d&uuml;l&uuml; ne olarak belirlendiyse, belirlenen s&uuml;rede bu &ouml;d&uuml;le sahip olacaksınız. T&uuml;m mantığı ile sistemimiz bu noktalar doğrultusunda farklılık g&ouml;stermektedir.</p>
+<ul>
+<li><strong>Fikir Bah&ccedil;ıvanı platformu hangi dillerde kullanılabilir? </strong></li>
+</ul>
+<p>&nbsp;İnternet sitemiz T&uuml;rk&ccedil;e ve İngilizce olarak yayımlanmıştır fakat İngilizce kısmı i&ccedil;in &ccedil;alışmalar hala devam etmektedir. Başlangı&ccedil; olarak internet sitesini T&uuml;rk&ccedil;e kullanmanız sistemi anlamanız a&ccedil;ısından daha iyi olacaktır.</p>
+<ul>
+<li><strong>Projem İngilizce dilinde de yayımlanabilir mi? </strong></li>
+</ul>
+<p>&nbsp;Projeniz denetim kurulu tarafından onaylandıktan sonra platformda projenizin bilgilerini İngilizce olarak g&ouml;nderirseniz, bu konu hakkında yardımcı olabiliriz.</p>
+	
+	
+	
+	
     <!--================ End About Us Area =================-->
 
 	<!--================Team Area =================-->
