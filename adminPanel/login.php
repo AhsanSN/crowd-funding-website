ea93a6d979c6f6b6105b8e76f46db68660bdc5e6
Fikir Bahçıvanı <51836340+fikirbahcivani@users.noreply.github.com>
2019-07-15 14:08:21 +0300

    Update login.php

--- adminPanel/login.php
+++ adminPanel/login.php
@@ -46,7 +46,7 @@ if(($email=='admin@admin.admin')&&($password=='123')){
 
 <body class="">
   <div class="wrapper ">
-    <div class="sidebar" data-color="purple" data-background-color="white" data-image="assets/img/sidebar-1.jpg">
+    <div class="sidebar" data-color="green" data-background-color="white" data-image="assets/img/sidebar-1.jpg">
       <!--
         Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
 
@@ -67,7 +67,7 @@ if(($email=='admin@admin.admin')&&($password=='123')){
               <div class="card">
                 <div class="card-header card-header-primary">
 
-                  <h4 class="card-title ">Admin Login</h4>
+                  <h4 class="card-title ">Giriş Yap</h4>
                 </div>
                 <div class="card-body">
                   <div class="table-responsive">
@@ -78,11 +78,11 @@ if(($email=='admin@admin.admin')&&($password=='123')){
                           <input name="email" type="email" class="form-control" placeholder="">
                         </div>
                         <div class="form-group col-md-6">
-                          <label for="inputEmail4">Password</label>
+                          <label for="inputEmail4">Parola</label>
                           <input name="password" type="password" class="form-control" placeholder="">
                         </div>
                       </div>
-                      <button type="submit" class="btn btn-primary">Sign in</button>
+                      <button type="submit" class="btn btn-primary">Giriş Yap</button>
                     </form>
                   </div>
                 </div>
